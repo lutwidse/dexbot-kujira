5c4e4cf30f6b2eb2fa2a7f2beb57af116f14ee22
Lutwidse <74334594+Lutwidse@users.noreply.github.com>
2022-05-06 07:24:28 +0900

    add type specification

--- orca_dexbot/anchor_protocol/money_market.py
+++ orca_dexbot/anchor_protocol/money_market.py
@@ -1,8 +1,13 @@
+import logging
+from terra_sdk.client.lcd import LCDClient
+from orca_dexbot import contract
+
+
 class Overseer:
     def __init__(self, _terra, _contract, _logger):
-        self._terra = _terra
-        self._contract = _contract
-        self._logger = _logger
+        self._terra: LCDClient = _terra
+        self._contract: contract = _contract
+        self._logger: logging = _logger
 
     def epoch_state(self):
         try:
