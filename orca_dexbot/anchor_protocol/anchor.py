0d54f92bb474290415227cda39762c0f48ebe0ef
Lutwidse <74334594+Lutwidse@users.noreply.github.com>
2022-05-06 11:23:41 +0900

    fix import

--- orca_dexbot/anchor_protocol/anchor.py
+++ orca_dexbot/anchor_protocol/anchor.py
@@ -1,5 +1,5 @@
-from money_market import Overseer, Market
-from liquidation import Liquidation
+from .money_market import Overseer, Market
+from .liquidation import Liquidation
 
 class Anchor():
     def __init__(self, _logger, _terra, _wallet, _contract, _wrapper):
