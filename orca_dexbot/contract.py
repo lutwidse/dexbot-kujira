21ed15d7d1acea7e196579f6b6304ddd1ad9b973
Lutwidse <74334594+Lutwidse@users.noreply.github.com>
2022-05-06 11:31:02 +0900

    update MainnetContract

--- orca_dexbot/contract.py
+++ orca_dexbot/contract.py
@@ -1,5 +1,11 @@
 class MainnetContract(object):
-    pass
+    ANCHOR_MARKET = "terra1sepfj7s0aeg5967uxnfk4thzlerrsktkpelm5s"
+    ANCHOR_AUST = "terra1ajt556dpzvjwl0kl5tzku3fc3p3knkg9mkv8jl"
+    ANCHOR_BLUNA = "terra1kc87mu460fwkqte29rquh4hc20m54fxwtsx7gp"
+
+    KUJIRA_ORCA_AUST = "terra13nk2cjepdzzwfqy740pxzpe3x75pd6g0grxm2z"
+
+
 class TestnetContract(object):
     ANCHOR_MARKET = "terra15dwd5mj8v59wpj0wvt233mf5efdff808c5tkal"
     ANCHOR_AUST = "terra1ajt556dpzvjwl0kl5tzku3fc3p3knkg9mkv8jl"
