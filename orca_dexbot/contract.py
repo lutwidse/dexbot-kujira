da63c4f984af74c46c27b6650f5271951e01bf37
Lutwidse <74334594+Lutwidse@users.noreply.github.com>
2022-05-06 16:34:38 +0900

    update contract.py

--- orca_dexbot/contract.py
+++ orca_dexbot/contract.py
@@ -6,6 +6,9 @@ class MainnetContract(object):
 
     KUJIRA_ORCA_AUST = "terra13nk2cjepdzzwfqy740pxzpe3x75pd6g0grxm2z"
 
+    ASTROPORT_BLUNA_LUNA = "terra1j66jatn3k50hjtg2xemnjm8s7y8dws9xqa5y8w"
+    ASTROPORT_LUNA_UST = "terra1m6ywlgn6wrjuagcmmezzz2a029gtldhey5k552"
+
 
 class TestnetContract(object):
     ANCHOR_MARKET = "terra15dwd5mj8v59wpj0wvt233mf5efdff808c5tkal"
@@ -14,3 +17,6 @@ class TestnetContract(object):
     ANCHOR_LIQUIDATION_QUEUE = "terra18j0wd0f62afcugw2rx5y8e6j5qjxd7d6qsc87r"
 
     KUJIRA_ORCA_AUST = "terra1k99xg89zr4exgzuklqdm9q4t5hunyw7tm2l4rl"
+
+    ASTROPORT_BLUNA_LUNA = "terra1esle9h9cjeavul53dqqws047fpwdhj6tynj5u4"
+    ASTROPORT_LUNA_UST = "terra1e49fv4xm3c2znzpxmxs0z2z6y74xlwxspxt38s"
