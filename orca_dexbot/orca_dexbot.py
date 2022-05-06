e9dad715bcb1c17b7b7ea08d9aa0848c2d437922
Lutwidse <74334594+Lutwidse@users.noreply.github.com>
2022-05-07 07:24:03 +0900

    change logger scope

--- orca_dexbot/orca_dexbot.py
+++ orca_dexbot/orca_dexbot.py
@@ -1,6 +1,7 @@
 import logging
 
-from pyparsing import token_map
+import base64
+from terra_sdk.core.wasm.msgs import MsgExecuteContract
 
 from .contract import MainnetContract, TestnetContract
 from .anchor_protocol.anchor import Anchor
@@ -19,16 +20,19 @@ from terra_sdk.core.broadcast import (
 COLUMBUS = ["https://lcd.terra.dev", "columbus-5"]
 BOMBAY = ["https://bombay-lcd.terra.dev/", "bombay-12"]
 
-logger = logging.getLogger(__name__)
-logger.setLevel(level=logging.DEBUG)
-handler = logging.FileHandler("debug.log")
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
-
-class OrcaDexbot:
+class OrcaDexbot(Anchor):
     def __init__(self, network, mnemonic):
+
+        self._logger = logging.getLogger(__name__)
+        self._logger.setLevel(level=logging.DEBUG)
+        handler = logging.FileHandler("debug.log")
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        self._logger.addHandler(handler)
+
         if network == "mainnet":
             self._terra = LCDClient(COLUMBUS[0], COLUMBUS[1])
             self._contract = MainnetContract()
@@ -39,9 +43,9 @@ class OrcaDexbot:
         self._wallet = self._terra.wallet(MnemonicKey(mnemonic=mnemonic))
         self._sequence = self._wallet.sequence()
 
-        self._wrapper = TerraWrapper(logger, self._terra, self._wallet, self._sequence)
+        self._wrapper = TerraWrapper(self._logger, self._terra, self._wallet, self._sequence)
         self._anchor = Anchor(
-            logger, self._terra, self._wallet, self._contract, self._wrapper
+            self._logger, self._terra, self._wallet, self._sequence, self._wrapper
         )
 
     def usd_uusd_conversion(
@@ -55,32 +59,32 @@ class OrcaDexbot:
             result = str(result)
         if is_need_prefix:
             result = result + "uusd"
-        logger.info(f"[usd_uusd_conversion] : {result}")
+        self._logger.info(f"[usd_uusd_conversion] : {result}")
         return result
 
     def get_native_token(self, wallet_address) -> Coins:  # (Coins, dict):
         try:
             result = self._terra.bank.balance(wallet_address)
-            logger.info(f"[get_native_token] : {result}")
+            self._logger.info(f"[get_native_token] : {result}")
             return result
         except:
-            logger.debug("[get_native_token]", exc_info=True, stack_info=True)
+            self._logger.debug("[get_native_token]", exc_info=True, stack_info=True)
 
     def get_cw_token(self, token_address) -> dict:
         try:
             query = {"balance": {"address": self._wallet.key.acc_address}}
             result = self._terra.wasm.contract_query(token_address, query)
-            logger.info(f"[get_cw_token] : {result}")
+            self._logger.info(f"[get_cw_token] : {result}")
             return result
         except:
-            logger.debug("[get_cw_token]", exc_info=True, stack_info=True)
+            self._logger.debug("[get_cw_token]", exc_info=True, stack_info=True)
 
     def get_bluna_contract(self) -> str:
         return self._contract.ANCHOR_BLUNA
 
     def _create_transaction(self, msgs) -> BlockTxBroadcastResult:
         try:
-            logger.info("[_create_transaction]", msgs)
+            self._logger.info("[_create_transaction]", msgs)
 
             tx = self._wallet.create_and_sign_tx(
                 CreateTxOptions(
@@ -95,7 +99,7 @@ class OrcaDexbot:
             result = self._terra.tx.broadcast(tx)
             return result
         except:
-            logger.debug("[_create_transaction]", stack_info=True)
+            self._logger.debug("[_create_transaction]", stack_info=True)
 
     def transaction_test(self, amount):
         msgs = [
@@ -106,7 +110,7 @@ class OrcaDexbot:
             )
         ]
         tx = self._wrapper._create_transaction(msgs)
-        logger.debug(f"[transaction_test] : {tx}")
+        self._logger.debug(f"[transaction_test] : {tx}")
 
     def transaction_anchor_deposit(self, amount):
         self.deposit_stable(amount)
